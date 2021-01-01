6e75cf0bca3f8f6d8afeeac70dd852e03311724c
James Go <jamescookgo@gmail.com>
2021-01-01 00:12:09 -0800

    unix timestamp function

--- other.go
+++ other.go
@@ -3,9 +3,14 @@ package gotools
 import (
 	"crypto/md5"
 	"fmt"
+	"time"
 )
 
 func Md5(s string) string {
 	b := []byte(s)
 	return fmt.Sprintf("%x", md5.Sum(b))
+}
+
+func Unix_timestamp() int {
+	return int(time.Now().Unix())
 }
